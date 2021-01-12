1d4311dfe569149e423ce094dadfe3ef9b671e29
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-01-12 20:48:06 +0800

    x SW

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,8 @@ import theme from "../theme/theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { useEffect } from "react";
-import { Workbox } from "workbox-window";
 
 function MyApp({ Component, pageProps }) {
-    useEffect(() => {
-        if (
-            !("serviceWorker" in navigator) ||
-            process.env.NODE_ENV !== "production"
-        ) {
-            console.warn("Progressive Web App support is disabled");
-            return;
-        }
-        const workbox = new Workbox("sw.js", { scope: "/" });
-        workbox.register();
-    }, []);
-
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
