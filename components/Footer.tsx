d7d436b9b395975955697ed1784c715069369102
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-02-20 23:20:25 +0800

    Alignment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,19 +5,19 @@ const Footer = () => {
         <>
             <footer className="py-12 flex justify-center items-center border-t-2 border-gray-300 dark:border-gray-600 bg-white dark:bg-black">
                 <div className="max-w-6xl mx-4">
-                    <div className="flex gap-4 space-x-4">
+                    <div className="flex flex-wrap">
                         <Link href="/privacy">
-                            <a className="text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
+                            <a className="m-2 md:mx-4 text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
                                 Privacy Policy
                             </a>
                         </Link>
                         <Link href="/terms">
-                            <a className="text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
+                            <a className="m-2 md:mx-4 text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
                                 Terms of Service
                             </a>
                         </Link>
                         <Link href="/contact">
-                            <a className="text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
+                            <a className="m-2 md:mx-4 text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
                                 Contact
                             </a>
                         </Link>
