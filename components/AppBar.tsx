c0bcdcfae6e2ab12ccce1776235a0efe17c9e940
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-04 17:01:13 +0800

    migrate to feli page ui

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,5 +1,5 @@
 import FeliIcon from "@/components/FeliIcon";
-import Link from "next/link";
+import { AppBar as AppBarBase } from "@felipage/react-ui";
 import React from "react";
 
 interface Props {
@@ -7,19 +7,7 @@ interface Props {
 }
 
 const AppBar = ({ title = "Feli Page" }: Props) => {
-    return (
-        <header className="min-h-56 md:min-h-64 flex justify-center items-center border-b-2 border-gray-300 dark:border-gray-600 bg-white dark:bg-black">
-            <div className="max-w-6xl w-full mx-4">
-                <div className="flex items-start justify-start space-x-6">
-                    <FeliIcon size={24} />
-                    <Link href="/">
-                        <a className="text-xl font-bold">{title}</a>
-                    </Link>
-                </div>
-                <div className=""></div>
-            </div>
-        </header>
-    );
+    return <AppBarBase title={title} Icon={FeliIcon} />;
 };
 
 export default AppBar;
