536415bac43a7f5ce840671b5e52a263c88ca1f7
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-04 17:03:12 +0800

    added my account button

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,13 +1,30 @@
 import FeliIcon from "@/components/FeliIcon";
-import { AppBar as AppBarBase } from "@felipage/react-ui";
+import { AppBar as AppBarBase, Button } from "@felipage/react-ui";
 import React from "react";
+import { useRouter } from "next/router";
 
 interface Props {
     title?: string;
 }
 
 const AppBar = ({ title = "Feli Page" }: Props) => {
-    return <AppBarBase title={title} Icon={FeliIcon} />;
+    const router = useRouter();
+
+    const goToMyAccount = () => {
+        router.push("https://accounts.feli.page");
+    };
+
+    return (
+        <AppBarBase
+            title={title}
+            Icon={FeliIcon}
+            actions={
+                <>
+                    <Button onClick={goToMyAccount}>My Account</Button>
+                </>
+            }
+        />
+    );
 };
 
 export default AppBar;
