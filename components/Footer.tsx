08a7208f1d0cd04ddfb7aaffd472a8ca01b9c84a
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-04-12 22:01:09 +0800

    uses library ui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,22 @@
 import Link from "next/link";
+import { Footer as FooterBase, FooterLink } from "@felipage/react-ui";
 
 const Footer = () => {
     return (
         <>
-            <footer className="py-12 flex justify-center items-center border-t-2 border-gray-300 dark:border-gray-600 bg-white dark:bg-black">
-                <div className="max-w-6xl mx-4">
-                    <div className="flex flex-wrap">
-                        <Link href="/privacy">
-                            <a className="m-2 md:mx-4 text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
-                                Privacy Policy
-                            </a>
-                        </Link>
-                        <Link href="/terms">
-                            <a className="m-2 md:mx-4 text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
-                                Terms of Service
-                            </a>
-                        </Link>
-                        <Link href="/contact">
-                            <a className="m-2 md:mx-4 text-feli-darker hover:text-feli-dark active:text-feli dark:text-feli-lighter dark:hover:text-feli-light dark:active:text-feli transition-colors">
-                                Contact
-                            </a>
-                        </Link>
-                    </div>
+            <FooterBase>
+                <div className="flex space-x-6">
+                    <Link href="/privacy" passHref>
+                        <FooterLink>Privacy Policy</FooterLink>
+                    </Link>
+                    <Link href="/terms" passHref>
+                        <FooterLink>Terms of Service</FooterLink>
+                    </Link>
+                    <Link href="/contact" passHref>
+                        <FooterLink>Contact</FooterLink>
+                    </Link>
                 </div>
-            </footer>
+            </FooterBase>
         </>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const plugin = require("tailwindcss/plugin");
 
 module.exports = {
     presets: [require("@felipage/tailwind-feli")],
+    mode: "jit",
     purge: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
