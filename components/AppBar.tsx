713fd55df2a2d39c9051dc931d0960027685f058
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-03-24 23:44:06 +0000

    migrate off felipage-ui

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,13 +1,16 @@
 import FeliIcon from "@/components/FeliIcon";
-import { AppBar as AppBarBase } from "@felipage/react-ui";
+import Link from "next/link";
 import React from "react";
 
-interface Props {
-  title?: string;
-}
-
-const AppBar = ({ title = "Feli Page" }: Props) => {
-  return <AppBarBase title={title} Icon={FeliIcon} actions={<></>} />;
+const AppBar = () => {
+  return (
+    <div className="flex min-h-14 items-center border-b">
+      <div className="container flex items-center gap-2">
+        <FeliIcon size={32} />
+        <Link href="/">Feli Page</Link>
+      </div>
+    </div>
+  );
 };
 
 export default AppBar;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,21 @@
 import Link from "next/link";
-import { Footer as FooterBase, FooterLink } from "@felipage/react-ui";
 
 const Footer = () => {
   return (
     <>
-      <FooterBase>
-        <div className="flex space-x-6">
-          <Link href="/privacy" passHref legacyBehavior>
-            <FooterLink>Privacy Policy</FooterLink>
+      <div className="border-t py-8">
+        <div className="container flex space-x-6">
+          <Link href="/privacy">
+            <>Privacy Policy</>
           </Link>
-          <Link href="/terms" passHref legacyBehavior>
-            <FooterLink>Terms of Service</FooterLink>
+          <Link href="/terms">
+            <>Terms of Service</>
           </Link>
-          <Link href="/contact" passHref legacyBehavior>
-            <FooterLink>Contact</FooterLink>
+          <Link href="/contact">
+            <>Contact</>
           </Link>
         </div>
-      </FooterBase>
+      </div>
     </>
   );
 };
