1de1194deac55983f47404e74a34d1d2a6286147
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-03-24 23:28:34 +0000

    migrate legal docs to app router

---
+++ app/(legal)/layout.tsx
@@ -0,0 +1,16 @@
+import Content from "@/components/Content";
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <Content justifyText>
+      <div className="my-8 py-8">
+        <article className="prose max-w-none dark:invert dark:filter">
+          {children}
+        </article>
+      </div>
+    </Content>
+  );
+};
+
+export default Layout;
