cb99fa4ba4e0e3c304f3d57885b02fae30e1e0f5
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-03-24 23:37:33 +0000

    remove pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.scss";
+import Layout from "@/components/Layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable}`}
       suppressHydrationWarning
     >
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <div id="app">
+          <Layout>{children}</Layout>
+        </div>
+      </body>
     </html>
   );
 }
--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,30 +1,13 @@
 import FeliIcon from "@/components/FeliIcon";
-import { AppBar as AppBarBase, Button } from "@felipage/react-ui";
+import { AppBar as AppBarBase } from "@felipage/react-ui";
 import React from "react";
-import { useRouter } from "next/router";
 
 interface Props {
   title?: string;
 }
 
 const AppBar = ({ title = "Feli Page" }: Props) => {
-  const router = useRouter();
-
-  const goToMyAccount = () => {
-    router.push("https://accounts.feli.page");
-  };
-
-  return (
-    <AppBarBase
-      title={title}
-      Icon={FeliIcon}
-      actions={
-        <>
-          <Button onClick={goToMyAccount}>My Account</Button>
-        </>
-      }
-    />
-  );
+  return <AppBarBase title={title} Icon={FeliIcon} actions={<></>} />;
 };
 
 export default AppBar;
