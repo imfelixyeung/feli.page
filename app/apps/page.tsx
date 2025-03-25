52d9299d9dccc14ee5b4f801d108101a80749cd7
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2025-03-25 00:00:31 +0000

    split home page into / and /apps

---
+++ app/apps/page.tsx
@@ -0,0 +1,36 @@
+import AppPreviewCard from "@/components/AppPreview/Card";
+import Content from "@/components/Content";
+import PageHead from "@/components/PageHead";
+import { Metadata } from "next";
+import projects from "projects/projects";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Apps",
+  description: "Feli Page Apps",
+};
+
+const Home = () => {
+  return (
+    <>
+      <PageHead />
+      <Content center>
+        <div className="my-10">
+          <h1 className="text-center text-2xl font-medium">Apps</h1>
+          <div className="flex flex-wrap justify-center">
+            {projects.map((project) => {
+              const { shortName: name, image, href, category } = project;
+              return (
+                <div className="m-2 inline-block md:m-4" key={href}>
+                  <AppPreviewCard {...{ name, image, href, category }} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </Content>
+    </>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,20 @@
-import AppPreviewCard from "@/components/AppPreview/Card";
 import Content from "@/components/Content";
 import FeliIcon from "@/components/FeliIcon";
-import PageHead from "@/components/PageHead";
-import projects from "projects/projects";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <PageHead />
       <Content center>
         <div className="mt-8 flex flex-col items-center">
           <FeliIcon margin={16} fixed />
-          <h1 className="text-center">
-            Welcome to <span className="text-feli">Feli.Page!</span>
+          <h1 className="text-center text-2xl font-medium">
+            Welcome to <span className="text-feli">Feli Page!</span>
           </h1>
-        </div>{" "}
-        <div className="my-10">
-          {/* <h1 className="ml-2 md:ml-4">Apps</h1> */}
-          <div className="flex flex-wrap justify-center">
-            {projects.map((project) => {
-              const { shortName: name, image, href, category } = project;
-              return (
-                <div className="m-2 inline-block md:m-4" key={href}>
-                  <AppPreviewCard {...{ name, image, href, category }} />
-                </div>
-              );
-            })}
-          </div>
+        </div>
+        <div className="mt-8">
+          <Link href="/apps">Browse Apps</Link>
         </div>
       </Content>
     </>
